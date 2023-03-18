4c8bebdd88cc5d53b2bb75b3847db88d1d1f545f
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-03-17 18:56:53 -0600

    request to DALL-E-2 API

---
+++ dalle.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+function dalle() {
+  const apiKey = "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";
+  const apiUrl = "https://api.openai.com/v1/images/generations";
+  let entrada = document.getElementById("text").value;
+
+  const params = {
+    prompt: `${entrada}`,
+    n: 1,
+    size: "1024x1024",
+    response_format: "url",
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(params),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const imageUrl = data.data[0].url;
+      const imageElement = document.createElement("img");
+      imageElement.src = imageUrl;
+      document.body.appendChild(imageElement);
+      console.log("funciona")
+    })
+    .catch((error) => console.log(error));
+}
